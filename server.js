47180cf1524a119be29f1c5188c20ce8d8ac1fb8
Aleena-Khan15 <alina.908000@gmail.com>
2026-05-09 18:18:28 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,6 @@ app.get('/', (req, res) => res.send('ForexTrendSense Backend Running'));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+    app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`));
   })
   .catch(err => console.log(err));
